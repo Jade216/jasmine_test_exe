d7282c67ce3f8b6afc0ac42849d7c33cb1760e37
jade <jadeli2166@yahoo.com>
2023-07-26 12:20:56 -0700

    finished test.js

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,26 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
+describe('loan calculator test', function () {
 
+  it('should calculate the monthly rate correctly', function () {
+    // ...
+    const values = {
+      amount: 150000,
+      years: 15,
+      rate: 3.5
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('1072.32')
+  });
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
+  it("should return a result with 2 decimal places", function () {
+    // ..
+    const values = {
+      amount: 150095,
+      years: 15,
+      rate: 3.5
+    }
+    expect(calculateMonthlyPayment(values)).toEqual('1073.00')
+  });
 
-/// etc
+  /// etc
+})
+
